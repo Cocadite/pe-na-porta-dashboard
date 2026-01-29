a5543565c6941fb211d1cafa3224bef093a5425c
Cocadite <larp6234970@gmail.com>
2026-01-29 12:54:35 -0300

    Delete api directory

--- api/index.js
+++
@@ -1,8 +0,0 @@
-let forms = [];
-export default async function handler(req,res){
-  if(req.method==='POST'){
-    forms.push(req.body);
-    return res.json({ ok:true });
-  }
-  res.json({ forms });
-}
